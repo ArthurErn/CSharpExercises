061aa15457f737984eb1352a5570be453df9e04c
sys-arthur <arthurern48@gmail.com>
2021-12-14 14:01:15 -0300

    exercise

---
+++ ConstructorProperties/ConstructorProperties/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Course {
+    class Produto {
+        private string _nome;
+        private readonly double _preco;
+        private read9ohint _quantidade;
+
+        public Produto() {
+        }
+        public Produto(string nome, double preco, int quantidade) {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+        public string Nome {
+            get { return _nome; }
+            set {
+                if (value != null && value.Length > 1) {
+                    _nome = value;
+                }
+            }
+        }
+        public double Preco {
+            get { return _preco; }
+        }
+        public int Quantidade {
+            get { return _quantidade; }
+        }
+    }
+}
