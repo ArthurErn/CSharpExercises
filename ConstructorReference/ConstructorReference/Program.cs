964d35b0f1a66872181f8010a2e7167aa50ab4fd
sys-arthur <arthurern48@gmail.com>
2021-12-14 10:58:50 -0300

    constructor exercise

---
+++ ConstructorReference/ConstructorReference/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConstructorReference {
+    class ProductEntity {
+        public string name;
+        public double price;
+        public int quantity;
+
+        //Default constructor, passing the value to the attribute
+        public ProductEntity() {
+            quantity = 0;
+        }
+
+        //overcharged constructor referencing default constructor with "this()"
+        public ProductEntity(string name, double price) : this() {
+            this.name = name;
+            this.price = price;
+        }
+
+        //overcharged constructor referencing another to reply it's values
+        public ProductEntity(string name, double price, int quantity): this(name, price) {
+            this.quantity = quantity;
+        }
+    }
+}
